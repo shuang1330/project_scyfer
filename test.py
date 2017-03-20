d88e2aa357cc30a512199d2091a6eab08efc99a9
shuang1330 <shuangli1330@gmail.com>
2017-03-20 17:55:08 +0100

    Tested with the git branches commands

--- test.py
+++ test.py
@@ -3,10 +3,10 @@ import numpy as np
 
 node1 = tf.constant(3.0,tf.float32)
 node2 = tf.constant(4.0)
-# print node1, node2
+print node1, node2
 
 sess = tf.Session()
-# print sess.run([node1,node2])
+print sess.run([node1,node2])
 
 node3 = tf.add(node1,node2)
 # print "node3: ",node3
